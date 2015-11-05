6ebd6352a5294b3aada668358e5f0a1fdc02ef11
Christoph Jerolimov <jerolimov+git+2015@gmail.com>
2015-11-05 23:49:12 +0100

    Generate image overview for all categories.

---
+++ generate_index.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+for folder in action alert av communication content device editor file hardware image maps navigation notification social toggle; do
+    echo Write $folder.html
+
+    echo "<html><body><h1>$folder</h1><table>" > $folder.html
+
+    for image in $(find "$folder/ios" -iname '*_black_*pt.png'); do
+        basename=$(basename $image)
+        echo "<tr><td><img src=$image></td><td>$basename</td></tr>"
+    done >> $folder.html
+
+    echo "</table><table style='color: white; background-color: black;'>" >> $folder.html
+
+    for image in $(find "$folder/ios" -iname '*_white_*pt.png'); do
+        basename=$(basename $image)
+        echo "<tr><td><img src=$image></td><td>$basename</td></tr>"
+    done >> $folder.html
+
+    echo "</table></body></html>" >> $folder.html
+done
