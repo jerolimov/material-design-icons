428748e029e94789fa865d9beb9d88ba7909c88d
Christoph Jerolimov <jerolimov+git+2015@gmail.com>
2015-11-06 00:04:54 +0100

    Improve script to unify android and ios icon filenames.

--- rename_files.sh
+++ rename_files.sh
@@ -7,9 +7,10 @@ for folder in action alert av communication content device editor file hardware 
     for imageset in $(find "$folder/ios" -iname '*.imageset'); do
         wrong_imageset=$(basename "$imageset" | sed 's/.imageset$//g')
         wrong_imageset_dir="$folder/ios/$wrong_imageset.imageset"
+
         echo "  Check imageset $wrong_imageset"
 
-        right_imageset=$(echo "$wrong_imageset" | sed 's/\(_[0-9][0-9]pt\)/_black\1/g;s/$/_black_24pt/g;s/white_black/white/g;s/black_black/black/g;s/pt_black_24pt$/pt/g')
+        right_imageset=$(echo "$wrong_imageset" | sed 's/\(_[0-9][0-9]pt\)/_black\1/g;s/$/_black_24pt/g;s/white_black/white/g;s/red_black/red/g;s/amber_black/amber/g;s/black_black/black/g;s/pt_black_24pt$/pt/g')
         right_imageset_dir="$folder/ios/$right_imageset.imageset"
 
         # If we have a no black dir but a white dir we create them from the current file
