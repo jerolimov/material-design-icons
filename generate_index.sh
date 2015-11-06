1ebd9eba24444966929d265f6130dc3201b34023
Christoph Jerolimov <jerolimov+git+2015@gmail.com>
2015-11-06 01:05:48 +0100

    Update image overview for all categories.

--- generate_index.sh
+++ generate_index.sh
@@ -7,28 +7,28 @@ for folder in action alert av communication content device editor file hardware 
 
     echo "<html><body><h1>$folder</h1><table>" > $folder.html
 
-    for image in $(find "$folder/ios" -iname '*_black_*pt.png'); do
+    for image in $(find "$folder/ios" -iname '*_black_*dp.png'); do
         basename=$(basename $image)
         echo "<tr><td><img src=$image></td><td>$basename</td></tr>"
     done >> $folder.html
 
     echo "</table><table style='color: black; background-color: lightgray;'>" >> $folder.html
 
-    for image in $(find "$folder/ios" -iname '*_amber_*pt.png'); do
+    for image in $(find "$folder/ios" -iname '*_amber_*dp.png'); do
         basename=$(basename $image)
         echo "<tr><td><img src=$image></td><td>$basename</td></tr>"
     done >> $folder.html
 
     echo "</table><table style='color: black; background-color: lightgray;'>" >> $folder.html
 
-    for image in $(find "$folder/ios" -iname '*_red_*pt.png'); do
+    for image in $(find "$folder/ios" -iname '*_red_*dp.png'); do
         basename=$(basename $image)
         echo "<tr><td><img src=$image></td><td>$basename</td></tr>"
     done >> $folder.html
 
     echo "</table><table style='color: white; background-color: black;'>" >> $folder.html
 
-    for image in $(find "$folder/ios" -iname '*_white_*pt.png'); do
+    for image in $(find "$folder/ios" -iname '*_white_*dp.png'); do
         basename=$(basename $image)
         echo "<tr><td><img src=$image></td><td>$basename</td></tr>"
     done >> $folder.html
